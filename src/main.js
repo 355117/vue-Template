b55af7cc4faecf730619613a5f2d34a9478a6013
用户名 <>
2023-01-22 08:30:25 +0800

    修复main.js冗余

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "@/router/index.js";
 import "normalize.css/normalize.css"
 import './assets/css/global.css'
 
 let app = createApp(App)
 
-app.use(router).mount("#app");
+app.mount("#app");
 
 
 
