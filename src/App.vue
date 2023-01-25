b0768f625af7ff88925a8219c5479827220e2bca
用户名 <>
2023-01-25 15:50:33 +0800

    优化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="App">
-    <h2>App</h2>
-  </div>
+    <h2 class="f">靓仔欢迎你</h2>
+    <img src="./assets/img/welcome.png" alt="欢迎你" style="width:300px;height: 300px;">
+    <div class="num">
+      <p>{{ count }}</p>
+    <button @click="add">+1</button></div>
+</div>
+ 
 </template>
 
 <script setup>
-
+import { ref } from 'vue'
+let count = ref(0)
+function add() {
+  count.value++
+}
 </script>
 
 <style lang="less" scoped>
 .App{
-    background-color: red;
+    padding: 0;
+    margin: 0;
+    overflow:hidden;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .f{
+      margin-bottom: 20px;
+    }
+    .num{
+      margin-top: 20px;
+      display: flex;
+      button{
+        margin-left: 10px;
+      }
+    }
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "normalize.css/normalize.css"
 import './assets/css/global.css'
 
 let app = createApp(App)
